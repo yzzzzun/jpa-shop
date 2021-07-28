8aa2a3b60d0d2e51782b06a901d6595985919881
youngjun.jin <yzzzzun@gmail.com>
2021-07-28 22:48:47 +0900

    feat: member 목록 조회 구현

--- src/main/java/com/yzzzzun/jpashop/api/MemberApiController.java
+++ src/main/java/com/yzzzzun/jpashop/api/MemberApiController.java
@@ -1,7 +1,11 @@
 package com.yzzzzun.jpashop.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -46,9 +50,38 @@ public class MemberApiController {
 		return new UpdateMemberResponse(memberId, findMember.getName());
 	}
 
+	@GetMapping("/api/v1/members")
+	public List<Member> membersV1() {
+		return memberService.findMembers();
+	}
+
+	@GetMapping("/api/v2/members")
+	public Result membersV2() {
+		List<MemberDto> memberDtos = memberService.findMembers().stream()
+			.map(member -> new MemberDto(member.getName()))
+			.collect(Collectors.toList());
+		return new Result(memberDtos.size(), memberDtos);
+	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	static class Result<T> {
+		private int count;
+		private T data;
+	}
+
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
+	static class MemberDto {
+		String name;
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+
 	static class UpdateMemberResponse {
 		private Long id;
 		private String name;
